cc36ebaff4003cb1bc2ffaa71dc0600e97922108
Jim McDonald <Jim@mcdee.net>
2021-01-19 00:37:38 +0000

    Add test filter.

---
+++ services/finalizer/standard/main_test.go
@@ -0,0 +1,29 @@
+// Copyright © 2021 Attestant Limited.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package standard_test
+
+import (
+	"os"
+	"testing"
+
+	"github.com/rs/zerolog"
+)
+
+func TestMain(m *testing.M) {
+	zerolog.SetGlobalLevel(zerolog.Disabled)
+	if os.Getenv("CHAINDB_DATABASE_URL") != "" &&
+		os.Getenv("ETH2CLIENT_ADDRESS") != "" {
+		os.Exit(m.Run())
+	}
+}
